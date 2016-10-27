9ca4ab117e8bc03b1c0732fc7ab319ea95e7d918
Night Moonlit <Night Moonlit>
2016-10-27 22:11:21 +0800

    io流

---
+++ read.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+int main(){
+    ifstream in;
+    cout << "请输入文件路径" << endl;
+    char path[100];
+    cin >> path;
+    in.open(path);
+    if(!in.is_open()){
+        cout << "Cloud not open the file: " << path << endl;
+        cout << "Program terminating." << endl;
+        exit(EXIT_FAILURE);
+    }
+    char data[1025];
+    while(in.good()){
+        in.getline(data,1024);
+        cout << data << endl;
+    }
+    in.close();
+    return 0;
+}
